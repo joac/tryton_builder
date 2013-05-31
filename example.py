3b10be6154be7d15e53fb234528ad836f9aadea2
Joaquín Sorianello <joac@gcoop.coop>
2013-05-31 17:37:20 -0300

    Moved and documented example

--- example.py
+++ example.py
@@ -1,19 +1,21 @@
+# Imports, for base primitives
 from tryton_builder import Module, Model, Field
 
+# We create a new module
 module = Module('HelloWorld')
 
+# Two models
 hello = Model('Hello', 'hello.hello')
 bye = Model('Bye', 'hello.Bye')
 
+# Add models to module
 module.add_model(hello)
 module.add_model(bye)
 
+# Add Fields
 hello.add_field(Field('Char', 'Name'))
 hello.add_field(Field('Char', 'Greeting'))
 
-#b.create_views()
-#b.create_module()
-#b.create_foo_bar()
-
-#b.build_all()
+# Build!!
 module.build()
+# Our module is placed on HelloWorld dir
